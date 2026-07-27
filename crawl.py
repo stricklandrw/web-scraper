61edb0fdc622fb6ab5a3509d20b8b1ab96b3c752
stricklandrw <stricklandrw@gmail.com>
2026-07-26 23:08:33 -0400

    Ch1: Setup, L5: Extract Links and IMages - Complete

--- crawl.py
+++ crawl.py
@@ -1,4 +1,4 @@
-from urllib.parse import urlsplit
+from urllib.parse import urlsplit, urljoin
 from bs4 import BeautifulSoup, Tag
 
 def normalize_url(url: str) -> str:
@@ -14,9 +14,45 @@ def get_heading_from_html(html: str) -> str:
 
 def get_first_paragraph_from_html(html: str) -> str:
     soup = BeautifulSoup(html, 'html.parser')
-    main_section = soup.find("main")
+    main_section = soup.find('main')
     if isinstance(main_section, Tag):
         first_p = main_section.find('p')
     else:
         first_p = soup.find('p')
     return first_p.get_text(strip=True) if isinstance(first_p, Tag) else ""
+
+def get_urls_from_html(html: str, base_url: str) -> list[str]:
+    urls = []
+    soup = BeautifulSoup(html, "html.parser")
+    anchors = soup.find_all("a")
+
+    for anchor in anchors:
+        if not isinstance(anchor, Tag):
+            continue
+        href = anchor.get("href")
+        if isinstance(href, str) and href:
+            try:
+                absolute_url = urljoin(base_url, href)
+                urls.append(absolute_url)
+            except Exception as e:
+                print(f"{str(e)}: {href}")
+
+    return urls
+
+def get_images_from_html(html: str, base_url: str) -> list[str]:
+    image_urls = []
+    soup = BeautifulSoup(html, "html.parser")
+    images = soup.find_all("img")
+
+    for img in images:
+        if not isinstance(img, Tag):
+            continue
+        src = img.get("src")
+        if isinstance(src, str) and src:
+            try:
+                absolute_url = urljoin(base_url, src)
+                image_urls.append(absolute_url)
+            except Exception as e:
+                print(f"{str(e)}: {src}")
+
+    return image_urls
--- test_crawl.py
+++ test_crawl.py
@@ -1,5 +1,5 @@
 import unittest
-from crawl import normalize_url
+from crawl import normalize_url, get_urls_from_html, get_images_from_html
 
 
 class TestCrawl(unittest.TestCase):
@@ -37,6 +37,58 @@ class TestCrawl(unittest.TestCase):
         input_url = "https://CRAWLER-TEST.com/path"
         actual = normalize_url(input_url)
         expected = "crawler-test.com/path"
+        self.assertEqual(actual, expected)
+
+    def test_get_urls_from_html_absolute(self):
+        input_url = "https://crawler-test.com"
+        input_body = '<html><body><a href="https://crawler-test.com"><span>Boot.dev</span></a></body></html>'
+        actual = get_urls_from_html(input_body, input_url)
+        expected = ["https://crawler-test.com"]
+        self.assertEqual(actual, expected)
+
+    def test_get_urls_from_html_relative(self) -> None:
+        input_url = "https://crawler-test.com"
+        input_body = '<html><body><a href="/logo.png"></body></html>'
+        actual = get_urls_from_html(input_body, input_url)
+        expected = ["https://crawler-test.com/logo.png"]
+        self.assertEqual(actual, expected)
+
+    def test_get_urls_from_html_no_links(self) -> None:
+        input_url = "https://crawler-test.com"
+        input_body = '<html><body><h1>Main Title</h1></body></html>'
+        actual = get_urls_from_html(input_body, input_url)
+        expected = []
+        self.assertEqual(actual, expected)
+
+    def test_get_images_from_html_relative(self):
+        input_url = "https://crawler-test.com"
+        input_body = '<html><body><img src="/logo.png" alt="Logo"></body></html>'
+        actual = get_images_from_html(input_body, input_url)
+        expected = ["https://crawler-test.com/logo.png"]
+        self.assertEqual(actual, expected)
+
+    def test_get_images_from_html_no_source(self):
+        input_url = "https://crawler-test.com"
+        input_body = '<html><body><img alt="Logo"></body></html>'
+        actual = get_images_from_html(input_body, input_url)
+        expected = []
+        self.assertEqual(actual, expected)
+
+    def test_get_images_from_html_multi_image(self):
+        input_url = "https://crawler-test.com"
+        input_body = '<html><body><img src="/logo.png" alt="Logo"><img src="/nike.png" alt="Nike"></body></html>'
+        actual = get_images_from_html(input_body, input_url)
+        expected = [
+            "https://crawler-test.com/logo.png",
+            "https://crawler-test.com/nike.png",
+        ]
+        self.assertEqual(actual, expected)
+
+    def test_get_images_from_html_absolute(self) -> None:
+        input_url = "https://crawler-test.com"
+        input_body = '<html><body><img src="https://crawler-test.com/logo.png" alt="Logo"></body></html>'
+        actual = get_images_from_html(input_body, input_url)
+        expected = ["https://crawler-test.com/logo.png"]
         self.assertEqual(actual, expected)
 
 if __name__ == "__main__":
